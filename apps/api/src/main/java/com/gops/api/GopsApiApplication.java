968d979c2b5a07d8f2b1cb21e0b7bfc877de14cf
RiteshPatel00 <patel2000ritesh@gmail.com>
2025-09-09 19:41:32 -0400

    Add in frontend and backend folder structure

---
+++ apps/api/src/main/java/com/gops/api/GopsApiApplication.java
@@ -0,0 +1,13 @@
+package com.gops.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GopsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GopsApiApplication.class, args);
+	}
+
+}
